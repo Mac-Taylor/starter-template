3bead4ccece083cf987aec0184ad1e2d05e993f9
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-04 16:22:27 -0400

    gulpfile is up to date

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,32 @@
-// gulpfile
+/**
+ * - Convert sass to css
+ * - bundle js modules (browserify)
+ * - strip comments
+ */
 
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browser = require('gulp-browser');
+
+let OUTPUT_DIRECTORY = 'public';
+
+// html, css, and js tasks
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('js/app.js')
+        .pipe(browser.browserify())
+        .pipe(gulp.dest('public'));
+});
+
+// watch
